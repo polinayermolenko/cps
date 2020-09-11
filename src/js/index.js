71527c263229a4ee66b83f948e464ebafb0825ea
Polina Yermolenko <polina.shvajukova@gmail.com>
2020-09-11 09:25:49 +0300

    Вёрстка и стилизация блока 1.3

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
 import "../scss/style.scss";
+
+const navListLinks = document.querySelectorAll(".nav-list__link");
+const aboutMoreButton = document.querySelector(".about-us__more-button");
+const textContainer = document.querySelector(".about-us__text-container");
+
+navListLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        navListLinks.forEach((link) => {
+        if (link.classList.contains("nav-list__link--active")) {
+          link.classList.remove("nav-list__link--active");
+        }
+      });
+  
+      link.classList.add("nav-list__link--active");
+    });
+  });
+  
+aboutMoreButton.addEventListener("click", () => {
+    if (aboutMoreButton.innerText === "Читать далее") {
+      aboutMoreButton.innerText = "Скрыть";
+      aboutMoreButton.classList.add("more-button--less");
+      textContainer.classList.add("about-us__text-container--full");
+    } else {
+      aboutMoreButton.innerText = "Читать далее";
+      aboutMoreButton.classList.remove("more-button--less");
+      textContainer.classList.remove("about-us__text-container--full");
+    }
+  });
