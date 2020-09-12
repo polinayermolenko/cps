3c2ca38f6579444c40b06b931344965571c01e87
Polina Yermolenko <polina.shvajukova@gmail.com>
2020-09-12 19:42:06 +0300

    Внесла правки по блоку 1.3

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,18 @@
 import "../scss/style.scss";
 
-const navListLinks = document.querySelectorAll(".nav-list__link");
+const navListLinks = document.querySelectorAll(".nav__link");
 const aboutMoreButton = document.querySelector(".about-us__more-button");
 const textContainer = document.querySelector(".about-us__text-container");
 
 navListLinks.forEach((link) => {
     link.addEventListener("click", () => {
         navListLinks.forEach((link) => {
-        if (link.classList.contains("nav-list__link--active")) {
-          link.classList.remove("nav-list__link--active");
+        if (link.classList.contains("nav__link--active")) {
+          link.classList.remove("nav__link--active");
         }
       });
   
-      link.classList.add("nav-list__link--active");
+      link.classList.add("nav__link--active");
     });
   });
   
