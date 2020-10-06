e3b3345b681c6d39f1fad15f3748ba91bde26234
Polina Yermolenko <polina.shvajukova@gmail.com>
2020-10-06 12:37:18 +0300

    Updates

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,16 @@
- 
-let swiperBrand;
-let isSwiperBrandActived = false;
 const TABLET = 768;
-const sliderBrand = document.querySelector('.swiper-container');
+const sliderBrand = document.querySelector('.swiper-container--brand');
+const sliderDevice = document.querySelector(".swiper-container--device");
+const sliderPrice = document.querySelector('.swiper-container--price');
 
+let swiperBrand;
+let swiperDevice;
+let swiperPrice;
+let isSwiperActived = false;
 
 const activateSwiper = () => {
   if (document.documentElement.clientWidth < TABLET) {
-    if (isSwiperBrandActived) {
+    if (isSwiperActived) {
         return false;
     }
     swiperBrand = new Swiper(sliderBrand, {
@@ -30,14 +33,53 @@ const activateSwiper = () => {
         }
        }
    });
-   isSwiperBrandActived = true;
+   swiperDevice = new Swiper(sliderDevice, {
+    slidesPerView: 1,
+    spaceBetween: 15,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  
+    breakpoints: {
+      460: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+  
+      660: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  swiperPrice = new Swiper(sliderPrice, {
+    slidesPerView: 1,
+    spaceBetween: 15,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  
+    breakpoints: {
+      460: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+    },
+  });
+   isSwiperActived = true;
   } else {
-    if (!isSwiperBrandActived) {
+    if (!isSwiperActived) {
         return false;
     }
     swiperBrand.destroy(true, true);
-    isSwiperBrandActived = false;
-}
-  }
- window.onload = activateSwiper;
-  window.onresize = activateSwiper;
+    swiperDevice.destroy(true, true);
+    swiperPrice.destroy(true, true);
+    isSwiperActived = false;
+ }
+};
+
+window.onload = activateSwiper;
+window.onresize = activateSwiper;
